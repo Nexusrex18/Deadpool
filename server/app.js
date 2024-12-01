9e18602ee7bdca4a40e9e68456786a0cd8a3d774
Nexusrex18 <154988813+Nexusrex18@users.noreply.github.com>
2024-12-01 16:27:30 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://deadpool-six.vercel.app/",
+    origin: "https://deadpool-six.vercel.app",
     credentials: true,
   })
 );
